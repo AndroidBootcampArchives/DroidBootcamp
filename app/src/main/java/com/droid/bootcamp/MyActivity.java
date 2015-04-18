c45bf3aa788e76d776be8bda84c4355077dc2970
NazneenRupawalla <nazneenr@thoughtworks.com>
2015-04-18 22:46:49 +0530

    Solution for assignment 1.

--- app/src/main/java/com/droid/bootcamp/MyActivity.java
+++ app/src/main/java/com/droid/bootcamp/MyActivity.java
@@ -3,16 +3,21 @@ package com.droid.bootcamp;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MyActivity extends Activity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-    public void add(View view){
-        TextView result = (TextView) findViewById(R.id.result);
-        result.setText("25");
-    }
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+
+  public void add(View view) {
+    int firstNumber = Integer.parseInt(((EditText) findViewById(R.id.number_input_one)).getText().toString());
+    int secondNumber = Integer.parseInt(((EditText) findViewById(R.id.number_input_two)).getText().toString());
+    int result = firstNumber + secondNumber;
+    TextView resultView = (TextView) findViewById(R.id.result);
+    resultView.setText(String.valueOf(result));
+  }
 }
